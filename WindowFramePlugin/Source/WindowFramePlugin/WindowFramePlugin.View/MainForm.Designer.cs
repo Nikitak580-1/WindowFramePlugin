ecd29582b633886febcc49d6986388d225d57585
Fix <nikish228xg@gmail.com>
2024-03-02 15:03:02 +0700

    feat(.View): Добавлены подписи областей. fix: Исправлена пс.

--- WindowFramePlugin/Source/WindowFramePlugin/WindowFramePlugin.View/MainForm.Designer.cs
+++ WindowFramePlugin/Source/WindowFramePlugin/WindowFramePlugin.View/MainForm.Designer.cs
@@ -49,12 +49,14 @@
             this.RangeTmLabel = new System.Windows.Forms.Label();
             this.BuildButton = new System.Windows.Forms.Button();
             this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.AllParametersLabel = new System.Windows.Forms.Label();
+            this.WindowParametersLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // TmTextBox
             // 
-            this.TmTextBox.Location = new System.Drawing.Point(209, 105);
+            this.TmTextBox.Location = new System.Drawing.Point(209, 131);
             this.TmTextBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.TmTextBox.Name = "TmTextBox";
             this.TmTextBox.Size = new System.Drawing.Size(100, 20);
@@ -64,7 +66,7 @@
             // 
             // W1TextBox
             // 
-            this.W1TextBox.Location = new System.Drawing.Point(209, 6);
+            this.W1TextBox.Location = new System.Drawing.Point(209, 32);
             this.W1TextBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.W1TextBox.Name = "W1TextBox";
             this.W1TextBox.Size = new System.Drawing.Size(100, 20);
@@ -74,7 +76,7 @@
             // 
             // G2TextBox
             // 
-            this.G2TextBox.Location = new System.Drawing.Point(209, 138);
+            this.G2TextBox.Location = new System.Drawing.Point(209, 164);
             this.G2TextBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.G2TextBox.Name = "G2TextBox";
             this.G2TextBox.Size = new System.Drawing.Size(100, 20);
@@ -84,7 +86,7 @@
             // 
             // H2TextBox
             // 
-            this.H2TextBox.Location = new System.Drawing.Point(209, 39);
+            this.H2TextBox.Location = new System.Drawing.Point(209, 65);
             this.H2TextBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.H2TextBox.Name = "H2TextBox";
             this.H2TextBox.Size = new System.Drawing.Size(100, 20);
@@ -94,7 +96,7 @@
             // 
             // ThTextBox
             // 
-            this.ThTextBox.Location = new System.Drawing.Point(209, 72);
+            this.ThTextBox.Location = new System.Drawing.Point(209, 98);
             this.ThTextBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.ThTextBox.Name = "ThTextBox";
             this.ThTextBox.Size = new System.Drawing.Size(100, 20);
@@ -104,7 +106,7 @@
             // 
             // L3TextBox
             // 
-            this.L3TextBox.Location = new System.Drawing.Point(209, 171);
+            this.L3TextBox.Location = new System.Drawing.Point(209, 197);
             this.L3TextBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.L3TextBox.Name = "L3TextBox";
             this.L3TextBox.Size = new System.Drawing.Size(100, 20);
@@ -115,7 +117,7 @@
             // W1Label
             // 
             this.W1Label.AutoSize = true;
-            this.W1Label.Location = new System.Drawing.Point(12, 9);
+            this.W1Label.Location = new System.Drawing.Point(12, 35);
             this.W1Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.W1Label.Name = "W1Label";
             this.W1Label.Size = new System.Drawing.Size(115, 13);
@@ -125,7 +127,7 @@
             // H2Label
             // 
             this.H2Label.AutoSize = true;
-            this.H2Label.Location = new System.Drawing.Point(12, 42);
+            this.H2Label.Location = new System.Drawing.Point(12, 68);
             this.H2Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.H2Label.Name = "H2Label";
             this.H2Label.Size = new System.Drawing.Size(118, 13);
@@ -135,7 +137,7 @@
             // ThLabel
             // 
             this.ThLabel.AutoSize = true;
-            this.ThLabel.Location = new System.Drawing.Point(12, 75);
+            this.ThLabel.Location = new System.Drawing.Point(12, 101);
             this.ThLabel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.ThLabel.Name = "ThLabel";
             this.ThLabel.Size = new System.Drawing.Size(153, 13);
@@ -145,7 +147,7 @@
             // TmLabel
             // 
             this.TmLabel.AutoSize = true;
-            this.TmLabel.Location = new System.Drawing.Point(12, 108);
+            this.TmLabel.Location = new System.Drawing.Point(12, 134);
             this.TmLabel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.TmLabel.Name = "TmLabel";
             this.TmLabel.Size = new System.Drawing.Size(168, 13);
@@ -155,7 +157,7 @@
             // G2Label
             // 
             this.G2Label.AutoSize = true;
-            this.G2Label.Location = new System.Drawing.Point(12, 141);
+            this.G2Label.Location = new System.Drawing.Point(12, 167);
             this.G2Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.G2Label.Name = "G2Label";
             this.G2Label.Size = new System.Drawing.Size(177, 13);
@@ -165,7 +167,7 @@
             // L3Label
             // 
             this.L3Label.AutoSize = true;
-            this.L3Label.Location = new System.Drawing.Point(12, 174);
+            this.L3Label.Location = new System.Drawing.Point(12, 200);
             this.L3Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.L3Label.Name = "L3Label";
             this.L3Label.Size = new System.Drawing.Size(191, 13);
@@ -175,7 +177,7 @@
             // RangeL3Label
             // 
             this.RangeL3Label.AutoSize = true;
-            this.RangeL3Label.Location = new System.Drawing.Point(320, 174);
+            this.RangeL3Label.Location = new System.Drawing.Point(320, 200);
             this.RangeL3Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.RangeL3Label.Name = "RangeL3Label";
             this.RangeL3Label.Size = new System.Drawing.Size(62, 13);
@@ -185,7 +187,7 @@
             // RangeG2Label
             // 
             this.RangeG2Label.AutoSize = true;
-            this.RangeG2Label.Location = new System.Drawing.Point(320, 141);
+            this.RangeG2Label.Location = new System.Drawing.Point(320, 167);
             this.RangeG2Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.RangeG2Label.Name = "RangeG2Label";
             this.RangeG2Label.Size = new System.Drawing.Size(62, 13);
@@ -195,7 +197,7 @@
             // RangeThLabel
             // 
             this.RangeThLabel.AutoSize = true;
-            this.RangeThLabel.Location = new System.Drawing.Point(320, 75);
+            this.RangeThLabel.Location = new System.Drawing.Point(320, 101);
             this.RangeThLabel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.RangeThLabel.Name = "RangeThLabel";
             this.RangeThLabel.Size = new System.Drawing.Size(50, 13);
@@ -205,7 +207,7 @@
             // RangeH2Label
             // 
             this.RangeH2Label.AutoSize = true;
-            this.RangeH2Label.Location = new System.Drawing.Point(320, 42);
+            this.RangeH2Label.Location = new System.Drawing.Point(320, 68);
             this.RangeH2Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.RangeH2Label.Name = "RangeH2Label";
             this.RangeH2Label.Size = new System.Drawing.Size(56, 13);
@@ -215,7 +217,7 @@
             // RangeW1Label
             // 
             this.RangeW1Label.AutoSize = true;
-            this.RangeW1Label.Location = new System.Drawing.Point(320, 9);
+            this.RangeW1Label.Location = new System.Drawing.Point(320, 35);
             this.RangeW1Label.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.RangeW1Label.Name = "RangeW1Label";
             this.RangeW1Label.Size = new System.Drawing.Size(56, 13);
@@ -225,7 +227,7 @@
             // RangeTmLabel
             // 
             this.RangeTmLabel.AutoSize = true;
-            this.RangeTmLabel.Location = new System.Drawing.Point(320, 108);
+            this.RangeTmLabel.Location = new System.Drawing.Point(320, 134);
             this.RangeTmLabel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
             this.RangeTmLabel.Name = "RangeTmLabel";
             this.RangeTmLabel.Size = new System.Drawing.Size(44, 13);
@@ -247,11 +249,31 @@
             // 
             this.PictureBox.BackgroundImage = global::WindowFramePlugin.Properties.Resources.схема;
             this.PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PictureBox.Location = new System.Drawing.Point(407, 6);
+            this.PictureBox.Location = new System.Drawing.Point(416, 25);
             this.PictureBox.Name = "PictureBox";
-            this.PictureBox.Size = new System.Drawing.Size(342, 302);
+            this.PictureBox.Size = new System.Drawing.Size(333, 283);
             this.PictureBox.TabIndex = 21;
             this.PictureBox.TabStop = false;
+            // 
+            // AllParametersLabel
+            // 
+            this.AllParametersLabel.AutoSize = true;
+            this.AllParametersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AllParametersLabel.Location = new System.Drawing.Point(3, 9);
+            this.AllParametersLabel.Name = "AllParametersLabel";
+            this.AllParametersLabel.Size = new System.Drawing.Size(177, 13);
+            this.AllParametersLabel.TabIndex = 22;
+            this.AllParametersLabel.Text = "Параметры для построения:";
+            // 
+            // WindowParametersLabel
+            // 
+            this.WindowParametersLabel.AutoSize = true;
+            this.WindowParametersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WindowParametersLabel.Location = new System.Drawing.Point(400, 9);
+            this.WindowParametersLabel.Name = "WindowParametersLabel";
+            this.WindowParametersLabel.Size = new System.Drawing.Size(146, 13);
+            this.WindowParametersLabel.TabIndex = 23;
+            this.WindowParametersLabel.Text = "Схема для построения:";
             // 
             // MainForm
             // 
@@ -259,6 +281,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(763, 315);
+            this.Controls.Add(this.WindowParametersLabel);
+            this.Controls.Add(this.AllParametersLabel);
             this.Controls.Add(this.PictureBox);
             this.Controls.Add(this.BuildButton);
             this.Controls.Add(this.RangeTmLabel);
@@ -313,6 +337,8 @@
         private System.Windows.Forms.Label RangeTmLabel;
         private System.Windows.Forms.Button BuildButton;
         private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Label AllParametersLabel;
+        private System.Windows.Forms.Label WindowParametersLabel;
     }
 }
 
